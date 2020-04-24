ce5268893226967a678b9f534b4e7604bd8514de
Diego Larrea <diego.larrea@konecta.com.py>
2020-04-24 19:22:40 -0400

    generated deployables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBeaVfKSRtsPLoHH_SLwzTn3GkODiJzfPw",
+    authDomain: "notes-dd84b.firebaseapp.com",
+    databaseURL: "https://notes-dd84b.firebaseio.com",
+    projectId: "notes-dd84b",
+    storageBucket: "notes-dd84b.appspot.com",
+    messagingSenderId: "646480195828",
+    appId: "1:646480195828:web:9cc4a4a065a041e583eea1"
+  }
 };
